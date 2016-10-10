82461d90011d70430b84b675ab08317e49918b0d
sreeranjanr <sreeranjan1991@gmail.com>
2016-10-10 15:39:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-    res.send(createTemplate(articleOne));
+    res.send(createTemplate(ArticleOne));
 });
 
 app.get('/article-two', function(req, res) {
