d0a08c4d03cdc5872497b9d9d275e682b1f40c58
sreeranjanr <sreeranjan1991@gmail.com>
2016-10-10 12:29:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,18 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send('this is article one');
-});
-
-app.get('/article-two', function (req, res) {
-  res.send('this is article two');
-});
-
-app.get('/article-three', function (req, res) {
-  res.send('this is article three');
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
